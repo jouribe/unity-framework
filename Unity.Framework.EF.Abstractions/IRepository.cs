ca4e412200ae550df059a5c1050a2e2738090515
Jorge O. Uribe <jouribepe@gmail.com>
2020-07-25 15:30:18 -0500

    Bulk Extension added.

--- Unity.Framework.EF.Abstractions/IRepository.cs
+++ Unity.Framework.EF.Abstractions/IRepository.cs
@@ -1,9 +1,24 @@
-﻿using URF.Core.Abstractions;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EFCore.BulkExtensions;
+using URF.Core.Abstractions;
 
 namespace Unity.Framework.EF.Abstractions
 {
     public interface IRepository<TEntity, TDbContext> : IRepository<TEntity> where TEntity : class
     {
-        //
+        void BulkInsert(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        void BulkUpdate(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        void BulkDelete(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        void BulkInsertOrUpdate(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        void BulkInsertOrUpdateOrDelete(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        void BulkRead(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkInsertAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkUpdateAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkDeleteAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkInsertOrUpdateAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkInsertOrUpdateOrDeleteAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
+        Task BulkReadAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null);
     }
 }
--- Unity.Framework.EF/Repository.cs
+++ Unity.Framework.EF/Repository.cs
@@ -1,15 +1,78 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
 using Unity.Framework.EF.Abstractions;
 using URF.Core.EF;
 
 namespace Unity.Framework.EF
 {
     public class Repository<TEntity, TDbContext> : Repository<TEntity>, IRepository<TEntity, TDbContext> where TDbContext : DbContext where TEntity: class
-
     {
-        public Repository(TDbContext context) : base(context)
+        protected Repository(TDbContext context) : base(context)
         {
             //
+        }
+
+        public void BulkInsert(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkInsert(entities, config, progress);
+        }
+
+        public void BulkUpdate(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkUpdate(entities, config, progress);
+        }
+
+        public void BulkDelete(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkDelete(entities, config, progress);
+        }
+
+        public void BulkInsertOrUpdate(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkInsertOrUpdate(entities, config, progress);
+        }
+
+        public void BulkInsertOrUpdateOrDelete(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkInsertOrUpdateOrDelete(entities, config, progress);
+        }
+
+        public void BulkRead(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            Context.BulkRead(entities, config, progress);
+        }
+
+        public async Task BulkInsertAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkInsertAsync(entities, config, progress);
+        }
+
+        public async Task BulkUpdateAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkUpdateAsync(entities, config, progress);
+        }
+
+        public async Task BulkDeleteAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkDeleteAsync(entities, config, progress);
+        }
+
+        public async Task BulkInsertOrUpdateAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkInsertOrUpdateAsync(entities, config, progress);
+        }
+
+        public async Task BulkInsertOrUpdateOrDeleteAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkInsertOrUpdateOrDeleteAsync(entities, config, progress);
+        }
+
+        public async Task BulkReadAsync(IList<TEntity> entities, BulkConfig config = null, Action<decimal> progress = null)
+        {
+            await Context.BulkReadAsync(entities, config, progress);
         }
     }
 }
